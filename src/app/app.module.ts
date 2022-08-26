959bdef43ccbb22282416e6c97be8d7cb8e67c4f
eiriselias <eiriselias@gmail.com>
2022-08-26 11:45:04 -0500

    Se creo el proyecto y el molde del este

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SkillsetComponent } from './components/skillset/skillset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortafolioComponent,
+    NavBarComponent,
+    SkillsetComponent
   ],
   imports: [
     BrowserModule
